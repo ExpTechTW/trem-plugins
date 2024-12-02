2412f13f607033e0cb404924041f640c411bc11b
YuYu1015 <whes1015@gmail.com>
2024-12-02 12:44:51 +0800

    fix: config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: "/TREM-Plugins",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
