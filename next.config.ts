fc81fe9fad86db26f98400c816920e8902df486a
YuYu1015 <whes1015@gmail.com>
2024-12-02 16:32:31 +0800

    fix: action

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === 'production';
+const basePath = isProduction ? '/TREM-Plugins' : '';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? "/TREM-Plugins" : "",
+  basePath: basePath,
+  assetPrefix: basePath,
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    if (isProduction) {
+      config.output.publicPath = `${basePath}/_next/`;
+    }
+    return config;
   }
 };
 
