6b414658e44d25e7ba7a4ad5d35eb55c00b11538
YuYu1015 <whes1015@gmail.com>
2024-12-02 12:30:18 +0800

    fix: actions

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const basePath = isProd ? '/TREM-Plugins' : '';
-
 const nextConfig: NextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: basePath,
-  assetPrefix: basePath,
-  trailingSlash: true,
 };
 
 export default nextConfig;
