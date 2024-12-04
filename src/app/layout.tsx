c55b1073435028bf865bf004710f908a2e0aa9b8
Kamiya <kamiya@kamiya.app>
2024-12-04 19:26:39 +0800

    chore: reduce card information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import '@fontsource/manrope/latin-400.css';
 import '@fontsource/manrope/latin-700.css';
 import '@fontsource/noto-sans-tc/400.css';
@@ -12,26 +12,18 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [mounted, setMounted] = useState(false);
-
   useEffect(() => {
     const darkMode = localStorage.getItem('darkMode');
     if (darkMode === 'true') {
       document.documentElement.classList.add('dark');
     }
-    setMounted(true);
+    else {
+      document.documentElement.classList.remove('dark');
+    }
   }, []);
 
-  if (!mounted) {
-    return (
-      <html lang="zh-Hant" suppressHydrationWarning>
-        <body>{children}</body>
-      </html>
-    );
-  }
-
   return (
-    <html lang="zh-Hant" suppressHydrationWarning>
+    <html lang="zh-Hant">
       <body>{children}</body>
     </html>
   );
--- src/components/plugin_card.tsx
+++ src/components/plugin_card.tsx
@@ -1,11 +1,13 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
 import { SiGithub } from '@icons-pack/react-simple-icons';
 import GithubPeople from './github_people';
-import { Download, Tag, Clock } from 'lucide-react';
-import { formatNumber, formatTimeString, getRelativeTime } from '@/lib/utils';
+import { ChevronDown, Download, Tag } from 'lucide-react';
+import { formatNumber } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
 
 import type { Plugin } from '@/modal/plugin';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu';
+import { Button } from './ui/button';
 
 interface Props {
   plugin: Plugin;
@@ -28,31 +30,8 @@ export default function PluginCard({ plugin }: Props) {
           <div className="flex flex-col gap-2">
             <CardTitle className="text-xl font-bold">{plugin.name}</CardTitle>
             <CardDescription>
-              <div className="flex flex-col gap-2">
+              <div>
                 <GithubPeople people={plugin.author} />
-                <div className="flex items-center gap-2 text-muted-foreground">
-                  <Clock size={16} className="shrink-0" />
-                  <div>
-                    <span>最後更新 </span>
-                    {plugin.repository.releases.releases[0]?.published_at
-                      ? (
-                          <div className="group relative inline-block">
-                            <span className="cursor-help hover:text-foreground transition-colors">
-                              {getRelativeTime(plugin.repository.releases.releases[0].published_at)}
-                            </span>
-                            <span className="invisible group-hover:visible absolute left-0 -top-8
-         bg-popover px-2.5 py-1.5 rounded-md text-sm shadow-md whitespace-nowrap
-         border z-10"
-                            >
-                              {formatTimeString(plugin.repository.releases.releases[0].published_at)}
-                            </span>
-                          </div>
-                        )
-                      : (
-                          <span>尚未發布</span>
-                        )}
-                  </div>
-                </div>
               </div>
             </CardDescription>
           </div>
@@ -86,6 +65,41 @@ export default function PluginCard({ plugin }: Props) {
               <span>{formatNumber(plugin.repository.releases.total_downloads)}</span>
             </div>
           </div>
+          {plugin.repository.releases.releases.length > 0
+          && (
+            <div className="flex" onClick={(e) => e.stopPropagation()}>
+              <Button className={plugin.repository.releases.releases.length > 1 ? 'rounded-e-none' : ''} asChild>
+                <a
+                  href={`https://github.com/${plugin.repository.full_name}/releases/latest/download/${plugin.name}.trem`}
+                  download
+                >
+                  下載
+                </a>
+              </Button>
+              {plugin.repository.releases.releases.length > 1
+              && (
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button size="icon" className="rounded-s-none w-6">
+                      <ChevronDown size={16} />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    {plugin.repository.releases.releases.map((release) => (
+                      <DropdownMenuItem key={release.tag_name} asChild>
+                        <a
+                          href={`https://github.com/${plugin.repository.full_name}/releases/download/${release.tag_name}/${plugin.name}.trem`}
+                          download
+                        >
+                          {release.tag_name}
+                        </a>
+                      </DropdownMenuItem>
+                    ))}
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              )}
+            </div>
+          )}
         </div>
       </CardFooter>
     </Card>
