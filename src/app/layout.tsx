362cc1eeddd7ea607d95bddc4251b2986d2f3deb
archie0732 <killer.archie.0732@gmail.com>
2024-12-05 10:44:21 +0800

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,13 @@ import '@fontsource/noto-sans-tc/700.css';
 import './globals.css';
 import { TooltipProvider } from '@/components/ui/tooltip';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'TREM 擴充',
+  description: 'TREM 擴充套件一覽',
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
