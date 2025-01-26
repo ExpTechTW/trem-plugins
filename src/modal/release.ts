4e92190fe1fa207640563ecda00b0e11811ec86b
YuYu1015 <whes1015@gmail.com>
2025-01-26 09:51:24 +0800

    fix: crash

--- src/modal/release.ts
+++ src/modal/release.ts
@@ -4,7 +4,7 @@ const Release = z.object({
   tag_name: z.string(),
   name: z.string(),
   downloads: z.number(),
-  published_at: z.string(),
+  published_at: z.string().nullable(),
 });
 
 type Release = z.infer<typeof Release>;
